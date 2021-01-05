2c5997c131ef6b3b8e6333dfe8f9bee0bbf64eb0
Alexis Degrugillier <aledeg@users.noreply.github.com>
2021-01-05 13:19:33 -0500

    Fix extension definition
    Before, extension was missing the definition of mandatory methods.
    At the moment, it's not a problem because the coding guidelines are not
    enforced by the code. But in the future, it will break.
    Now, extension have all mandatory method definitions.

--- xExtension-FixedNavMenu/extension.php
+++ xExtension-FixedNavMenu/extension.php
@@ -1,10 +1,20 @@
 <?php
 
 class FixedNavMenuExtension extends Minz_Extension {
+    public function install() {
+        return true;
+    }
+
+    public function uninstall() {
+        return true;
+    }
+
+    public function handleConfigureAction() {
+    }
 
     public function init() {
         Minz_View::appendStyle($this->getFileUrl('style.css', 'css'));
-        
+
         Minz_View::appendScript($this->getFileUrl('jquerymin.js', 'js'),'','','');
         Minz_View::appendScript($this->getFileUrl('script.js', 'js'),'','','');       
     }
