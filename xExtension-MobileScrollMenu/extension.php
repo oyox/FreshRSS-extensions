888ec512781cda3b2a4f4a8a6bbc85c0f8815536
Alexis Degrugillier <aledeg@users.noreply.github.com>
2021-01-05 13:21:25 -0500

    Fix extension definition
    Before, extension was missing the definition of mandatory methods.
    At the moment, it's not a problem because the coding guidelines are not
    enforced by the code. But in the future, it will break.
    Now, extension have all mandatory method definitions.

--- xExtension-MobileScrollMenu/extension.php
+++ xExtension-MobileScrollMenu/extension.php
@@ -1,10 +1,19 @@
 <?php
 
 class MobileScrollMenuExtension extends Minz_Extension {
+    public function install() {
+        return true;
+    }
+
+    public function uninstall() {
+        return true;
+    }
+
+    public function handleConfigureAction() {
+    }
 
     public function init() {
         Minz_View::appendScript($this->getFileUrl('jquerymin.js', 'js'),'','','');
         Minz_View::appendScript($this->getFileUrl('script.js', 'js'),'','','');   
     }
-
 }
